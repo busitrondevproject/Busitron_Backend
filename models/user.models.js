1cf87d6e6b2cad893c8a2192f26a405b63f7a911
lokamcharan2703 <l.charan@busitron.com>
2025-02-13 15:47:00 +0530

    add default avatar to usermodel

--- models/user.models.js
+++ models/user.models.js
@@ -22,7 +22,7 @@ const userSchema = new Schema(
 		avatar: {
 			type: String,
 			required: false,
-			defaultAvatar: "",
+			defaultAvatar: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
 		},
 		password: {
 			type: String,
