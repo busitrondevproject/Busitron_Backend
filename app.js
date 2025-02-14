76c7e0fee1acd7f299e965fe497a92b9d0cbb066
BalajiGanta <gantamanikantabalaji@busitron.com>
2025-02-14 16:08:10 +0530

    implemented contact page backend

--- app.js
+++ app.js
@@ -10,13 +10,17 @@ app.use(cookieParser());
 
 app.use(
 	cors({
-		origin: ["http://localhost:5173","http://localhost:5174","http://localhost:5175"],
+		origin: "*",
 		credentials: true,
 	})
 );
 
 import authRouter from "./route/auth.route.js";
+import contactRouter from "./route/contact.route.js"; 
+
 
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/contact", contactRouter);
+
 
 export { app };
