e00ea2f31f4c62f7b77de489640acd6b28076fcc
Vasudeva8278 <111454632+Vasudeva8278@users.noreply.github.com>
2025-02-14 18:43:01 +0530

    formatttt

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
 
 const app = express();
 
@@ -9,7 +8,6 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-dotenv.config()
 app.use(
 	cors({
 		origin: ["http://localhost:5173","http://localhost:5174","http://localhost:5175"],
