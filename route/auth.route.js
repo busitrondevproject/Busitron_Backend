82cbbdd231b44f256b6b9221ddcf30bc64469f84
lokamcharan2703 <l.charan@busitron.com>
2025-02-17 17:47:14 +0530

    added forgot password

--- route/auth.route.js
+++ route/auth.route.js
@@ -3,13 +3,15 @@
 import express from "express";
 import {
 	changePassword,
+	forgotPassword,
 	loginUser,
 	otpVerification,
+	passwordInvitation,
 	profileUpdate,
 	registerUser,
 	resendOtp,
 } from "../controller/auth.controller.js";
-import authenticateUser from "../middlewares/authMiddleware.js";
+import authenticateUser from "../middlewares/authMiddleware.js"
 
 const router = express.Router();
 
@@ -23,6 +25,10 @@ router.post("/re-sendOtp", authenticateUser, resendOtp);
 router.put("/profileUpdate", authenticateUser, profileUpdate);
 
 router.put("/changePassword", authenticateUser, changePassword);
+
+router.post("/forgot_invitation",authenticateUser,passwordInvitation);
+
+router.post("/forgot_password",authenticateUser,forgotPassword)
 
 router.get("/profile", authenticateUser, (req, res) => {
 	res.status(200).json({
